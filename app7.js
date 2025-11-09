6920c98782fd585564048af494815452712493b1
Jensen1109 <rodriguezjensen11@gmail.com>
2025-11-09 13:16:25 -0500

    se arreglo el ejercicio num 7 donde se le pide al usuario que el mismo digite el numero par ver si es par o impar

--- app7.js
+++ app7.js
@@ -1,11 +1,16 @@
-for(let i = 1; i<=10; i++)
-{
-    if(i % 2 == 0)
-    {
-        alert(`El numero ${i} es par`)
-    }
-    else
-    {
-        alert(`El numero ${i} es impar`)
+// Se inicia un ciclo for que recorre los numeros del 1 al 10
+for (let i = 1; i <= 10; i++) {
+
+    let n = parseInt(prompt("Ingrese un numero del 1 al 10;"))
+    // Se verifica si el numero actual es divisible por 2
+    if (n % 2 == 0) {
+
+        // Si el residuo es cero, el numero es par
+        alert(`El numero ${n} es par`);
+
+    } else {
+
+        // Si el residuo no es cero, el numero es impar
+        alert(`El numero ${n} es impar`);
     }
 }
