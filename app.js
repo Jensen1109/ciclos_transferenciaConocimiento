46c99363535b1ef8b9c740ed5b45478b10d6ec83
Jensen1109 <rodriguezjensen11@gmail.com>
2025-11-08 20:47:29 -0500

    se realizo el ejercicio 2 donde se hace la tabla de multiplicar hasta el 12 y si el usuario desea repetir la tabla se puede

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-// Inicia un bucle que recorre los números del 0 al 50
+// Recorremos los números del 2 al 50 (1 no es primo)
 for(let i = 2; i<=50; i++)
 {
+    // Suponemos que el número actual es primo
     let esprimo = true;
 
+    // Recorremos todos los posibles divisores menores que el número actual
     for(let j = 2 ; j<i;j++)
     {
+        // Si encontramos un divisor exacto, el número no es primo
         if(i % j  == 0)
         {
-            esprimo = false;
-            break;
+            esprimo = false;  // Marcamos que no es primo
+            break;  // Salimos del ciclo interno para ahorrar tiempo
         }
     }
+    // Si no se encontró ningún divisor, confirmamos que es primo
     if(esprimo){
-        alert(`numero par: ${i}`)
+        // Mostramos el número primo
+        alert(`numero primo: ${i}`)
     }
 }
-// Muestra una alerta final indicando que el proceso ha terminado
+// Mensaje final indicando que se terminó el proceso
 alert("Se acabaron los numeros primos")
---
+++ app2.js
@@ -0,0 +1,32 @@
+// Declaramos una variable de control para repetir el proceso
+let repetir = true;
+// Iniciamos un ciclo que se ejecuta mientras el usuario quiera seguir consultando tablas
+while(repetir)
+{
+    // Solicitamos al usuario que ingrese un número
+    let numero = parseInt(prompt("Ingrese un numero para ver su tabla"))
+
+// Verificamos que el valor ingresado sea un número válido
+    if(!isNaN(numero))
+    {
+        // Recorremos los números del 1 al 12 para generar la tabla de multiplicar
+        for(let i = 1; i<=12; i++)
+        {
+            // Recorremos los números del 1 al 12 para generar la tabla de multiplicar
+            alert(`${numero} = ${numero*i}`)
+        }
+    }
+    else
+    {
+        // Si el valor ingresado no es válido, mostramos un mensaje de error
+        alert("Ingrese un numero valido")
+    }
+
+    let respuesta = prompt("Desea consultar otra tabla ¿si o no?")
+
+    if (respuesta !== "si" && respuesta !== "SI")
+    {
+        repetir = false;
+        alert("Gracias por visitarnos")
+    }
+}
